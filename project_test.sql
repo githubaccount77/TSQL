c29b1898252d52db1951c0190eaee76961e698e0
githubaccount77 <christophertubman@outlook.com>
2023-06-23 14:00:41 -0400

    CT Changelog 6/23/23

--- project_test.sql
+++ project_test.sql
@@ -3,6 +3,7 @@ use FirstDB
 
 --Can Use column names
 --But must identify the specific column name instead of * for all you'd use select StudentId, FirstName and so on this is filtering the columns...
+--Expressions https://www.tutorialspoint.com/sql/sql-expressions.htm https://youtu.be/nHxi9oIQhHA?list=PLWPirh4EWFpFNzoF4EuFPWRjnTNSLyqI3
 
 
 select *
