a4fa8a9b94828bf70b0f1655cbf9c5cf95fa2607
githubaccount77 <christophertubman@outlook.com>
2023-06-23 14:35:46 -0400

    CT Changelog 6/23/23

--- project_test.sql
+++ project_test.sql
@@ -3,7 +3,16 @@ use FirstDB
 
 --Can Use column names
 --But must identify the specific column name instead of * for all you'd use select StudentId, FirstName and so on this is filtering the columns...
---Expressions https://www.tutorialspoint.com/sql/sql-expressions.htm https://youtu.be/nHxi9oIQhHA?list=PLWPirh4EWFpFNzoF4EuFPWRjnTNSLyqI3
+
+--Expressions https://www.tutorialspoint.com/sql/sql-expressions.htm 
+
+-- https://learn.microsoft.com/en-us/sql/t-sql/language-elements/expressions-transact-sql?view=sql-server-ver16
+
+-- https://youtu.be/nHxi9oIQhHA?list=PLWPirh4EWFpFNzoF4EuFPWRjnTNSLyqI3
+
+--T-SQL Operators https://learn.microsoft.com/en-us/sql/t-sql/language-elements/operators-transact-sql?view=sql-server-ver16
+
+
 
 
 select *
