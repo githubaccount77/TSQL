e3d466da2ca799d4829471ffca06a9a1b7274b1e
githubaccount77 <christophertubman@outlook.com>
2023-06-23 12:52:22 -0400

    CT Changelog 6/23/23

--- project_test.sql
+++ project_test.sql
@@ -1,5 +1,10 @@
 use FirstDB
 
+
+--Can Use column names
+--But must identify the specific column name instead of * for all you'd use select StudentId, FirstName and so on this is filtering the columns...
+
+
 select *
-from Employees
+from tbl_Students
 
