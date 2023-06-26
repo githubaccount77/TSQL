eeecfe7b81e45eb1c03441901eba680d055a3b05
githubaccount77 <christophertubman@outlook.com>
2023-06-26 14:01:04 -0400

    CT Changelog 6/26/23

--- project_test.sql
+++ project_test.sql
@@ -12,6 +12,22 @@ use FirstDB
 
 --T-SQL Operators https://learn.microsoft.com/en-us/sql/t-sql/language-elements/operators-transact-sql?view=sql-server-ver16
 
+-- SELECT - ORDER BY Clause (Transact-SQL) 
+-- https://learn.microsoft.com/en-us/sql/t-sql/queries/select-order-by-clause-transact-sql?view=sql-server-ver16
+-- https://youtu.be/3Y5KFsWT-m8?list=PLWPirh4EWFpFNzoF4EuFPWRjnTNSLyqI3&t=228
+
+-- T-SQL Subqueries 
+-- https://youtu.be/hcAKLo-cb0c?list=PLWPirh4EWFpFNzoF4EuFPWRjnTNSLyqI3
+
+-- Correlated Subquery 
+-- https://youtu.be/TXFpmvX5bGE?list=PLWPirh4EWFpFNzoF4EuFPWRjnTNSLyqI3&t=36
+
+-- T-SQL Joins
+--  https://www.tutorialspoint.com/t_sql/t_sql_joining_tables.htm
+-- https://youtu.be/Q1mtU_SoJIk?list=PLWPirh4EWFpFNzoF4EuFPWRjnTNSLyqI3
+
+
+
 
 
 
